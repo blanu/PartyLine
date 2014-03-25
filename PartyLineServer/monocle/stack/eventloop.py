9ee059253bb97248428169b9ac7a64d2eed07b03
Brandon Wiley <brandon@blanu.net>
2014-03-25 17:12:36 -0500

    Split Haskell and Python code

--- PartyLineServer/monocle/stack/eventloop.py
+++
@@ -1,7 +0,0 @@
-import monocle
-if monocle._stack_name == 'twisted':
-    from monocle.twisted_stack.eventloop import *
-elif monocle._stack_name == 'tornado':
-    from monocle.tornado_stack.eventloop import *
-elif monocle._stack_name == 'asyncore':
-    from monocle.asyncore_stack.eventloop import *
